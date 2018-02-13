e042e7fa5abd281fe5f47672dd3bd9f1d35b9f49
Phil Barker <phil@pjjk.net>
2018-02-13 18:22:56 +0000

    function and shortcode to display only taxons appropriate to page / post

--- inc/display_functions.php
+++ inc/display_functions.php
@@ -38,4 +38,21 @@ function wdtax_list_terms( $atts ) {
 add_shortcode( 'wdtax_terms', 'wdtax_list_terms' );
 add_filter('widget_text', 'do_shortcode');
 
+function wdtax_post_terms( $atts ) {
+// creates a shortcode wdtax_post_terms that requires one parameter
+// custom_taxonomy.
+// To use this shortcode drag and drop a Text widget into your sidebar.
+// Add this shortcode in your Widget and save.
+// [wdtax_post_terms custom_taxonomy=customtaxonomyname]
+// based on http://www.wpbeginner.com/plugins/how-to-display-custom-taxonomy-terms-in-wordpress-sidebar-widgets/
+  extract( shortcode_atts( array( 'custom_taxonomy' => '', ), $atts ) );
+//  global $post;
+//  $id = $post->ID;
+  $id = get_the_ID();
+  $widgettext = get_the_term_list($id, $custom_taxonomy, ' ', ' ', ' ');
+  return $widgettext;
+}
+add_shortcode( 'wdtax_post_terms', 'wdtax_post_terms' );
+
+
 ?>
